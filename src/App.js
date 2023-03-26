b0e644671508a653ec0b5d09e18081e203d650fe
dhiraj dubey <dhirajdubey19965@gmail.com>
2023-03-26 17:57:53 +0530

    csv file added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
+import SampleTemplate from "./Pages/SampleTemplate";
+import Error404 from "./Pages/Error404";
+import Csvtojson from './Pages/csvTojson/Csvtojson';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/sampleTemplate" element={<SampleTemplate />} />
+        <Route path="/csvTojson" element={<Csvtojson />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/SampleTemplate/index.jsx
@@ -0,0 +1,175 @@
+import { Button, Form, Upload } from "antd";
+import {
+  UploadOutlined,
+  RedoOutlined,
+  DeleteOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import "../../Styles/sampleTemplate.css";
+
+const SampleTemplate = () => {
+  const [fileList, setFileList] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [images, setImages] = useState([]);
+  const [refreshLoading, setRerfreshLoading] = useState(false);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = () => {
+    setRerfreshLoading(true);
+    setTimeout(() => {
+      fetch("http://localhost:8080/images")
+        .then((response) => response.json())
+        .then((data) => {
+          setImages(data);
+          console.log(data);
+          setRerfreshLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setRerfreshLoading(false);
+        });
+    },1000);
+  };
+
+  const handleDeleteImage = (id, image) => {
+    fetch(`http://localhost:8080/images/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setImages(images.filter((image) => image.id !== id));
+        if (image === selectedImage) {
+          setSelectedImage(null);
+        }
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <div className="sampleTemplateContainer">
+      <div className="sampleTemplateMainSection">
+        <div className="templateUploadSection">
+          <Form
+            onFinish={(values) => {
+              console.log({ values });
+              console.log(fileList);
+            }}
+            layout="inline"
+          >
+            <Form.Item
+              label="Sample Template"
+              name={"sampleTemplate"}
+              valuePropName="fileList"
+              getValueFromEvent={(event) => {
+                return event?.fileList;
+              }}
+              rules={[
+                {
+                  required: true,
+                  message: "Please upload the sample template",
+                },
+                {
+                  validator(_, fileList) {
+                    return new Promise((resolve, reject) => {
+                      if (fileList && fileList[0]?.size > 1000000) {
+                        reject("File size exceeded");
+                      } else {
+                        resolve("Success");
+                      }
+                    });
+                  },
+                },
+              ]}
+            >
+              <Upload
+                maxCount={1}
+                beforeUpload={(file) => {
+                  return new Promise((resolve, reject) => {
+                    if (file.size > 1000000) {
+                      reject("File size exceeded");
+                    } else {
+                      resolve("Success");
+                    }
+                  });
+                }}
+                customRequest={(info) => {
+                  setFileList([info.file]);
+                }}
+                showUploadList={false}
+                accept=".png,.jpeg,.jpg"
+              >
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+              </Upload>
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              Add Template
+            </Button>
+          </Form>
+          {fileList[0]?.name}
+        </div>
+        <div className="fullSizeTemplateImageContainer">
+          {selectedImage && (
+            <div className="fullSizeTemplateImage">
+              <img
+                src={selectedImage}
+                alt={selectedImage}
+                width={"90%"}
+                // onClick={() => setSelectedImage(null)}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* Right Section */}
+
+      <div className="sampleTemplateRightSection">
+        <div className="refreshButtonDiv">
+          <Button
+            type="primary"
+            icon={refreshLoading ? <LoadingOutlined /> : <RedoOutlined />}
+            onClick={getImages}
+          >
+            Refresh
+          </Button>
+        </div>
+        <div className="sampleTemplateImages">
+          {images.map((e, i) => (
+            <div key={e.id} className="templateImageContainer">
+              <img
+                src={e.image}
+                alt={e.image}
+                width={"100%"}
+                onClick={() => handleImageClick(e.image)}
+              />
+              <div
+                className="templateImageDeleteIcon"
+                onClick={() => handleDeleteImage(e.id, e.image)}
+              >
+                <DeleteOutlined />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SampleTemplate;
