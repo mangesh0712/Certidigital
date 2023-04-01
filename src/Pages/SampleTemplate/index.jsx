988c8da692b7bddcbdb923428f73d83b8ed78cb5
Pankaj Kumar Ram <pankajkr885@gmail.com>
2023-04-02 00:24:53 +0530

    backend integration started

--- src/Pages/SampleTemplate/index.jsx
+++ src/Pages/SampleTemplate/index.jsx
@@ -14,65 +14,82 @@ const SampleTemplate = () => {
   const [images, setImages] = useState([]);
   const [refreshLoading, setRerfreshLoading] = useState(false);
 
-  useEffect(() => {
-    getImages();
-  }, []);
+  // useEffect(() => {
+  //   getImages();
+  // }, []);
 
-  const getImages = () => {
-    setRerfreshLoading(true);
-    setImages([]);
-    setTimeout(() => {
-      fetch("http://localhost:8080/images")
-        .then((response) => response.json())
-        .then((data) => {
-          setImages(data);
-          console.log(data);
-          setRerfreshLoading(false);
-        })
-        .catch((error) => {
-          console.error(error);
-          setRerfreshLoading(false);
-        });
-    }, 1000);
-  };
+  // const getImages = () => {
+  //   setRerfreshLoading(true);
+  //   setImages([]);
+  //   setTimeout(() => {
+  //     fetch("http://localhost:8080/images")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setImages(data);
+  //         console.log(data);
+  //         setRerfreshLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //         setRerfreshLoading(false);
+  //       });
+  //   }, 1000);
+  // };
 
-  const handleDeleteImage = (id, image) => {
-    fetch(`http://localhost:8080/images/${id}`, {
-      method: "DELETE",
+  // const handleDeleteImage = (id, image) => {
+  //   fetch(`http://localhost:8080/images/${id}`, {
+  //     method: "DELETE",
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setImages(images.filter((image) => image.id !== id));
+  //       if (image === selectedImage) {
+  //         setSelectedImage(null);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was a problem with the fetch operation:", error);
+  //     });
+  // };
+
+  // const handleImageClick = (image) => {
+  //   setSelectedImage(image);
+  // };
+
+  //localhost:8080/template/uploadtemplate
+
+  // 'http://localhost:8080/template/uploadtemplate'
+
+  const handleSubmitForm = (values) => {
+    console.log("values: ", values);
+    const formData = new FormData();
+    // formData.append("image", values.sampleTemplate[0].originFileObj);
+    formData.append("image", values.sampleTemplate[0]);
+
+    fetch("http://localhost:8080/template/uploadtemplate", {
+      method: "POST",
+      body: formData,
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
+      .then((response) => response.text())
       .then((data) => {
         console.log(data);
-        setImages(images.filter((image) => image.id !== id));
-        if (image === selectedImage) {
-          setSelectedImage(null);
-        }
       })
       .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
+        console.error("error",error);
       });
-  };
-
-  const handleImageClick = (image) => {
-    setSelectedImage(image);
   };
 
   return (
     <div className="sampleTemplateContainer">
       <div className="sampleTemplateMainSection">
         <div className="templateUploadSection">
-          <Form
-            onFinish={(values) => {
-              console.log({ values });
-              console.log(fileList);
-            }}
-            layout="inline"
-          >
+          <Form onFinish={handleSubmitForm} layout="inline">
             <Form.Item
               label="Sample Template"
               name={"sampleTemplate"}
@@ -88,7 +105,7 @@ const SampleTemplate = () => {
                 {
                   validator(_, fileList) {
                     return new Promise((resolve, reject) => {
-                      if (fileList && fileList[0]?.size > 1000000) {
+                      if (fileList && fileList[0]?.size > 2000000) {
                         reject("File size exceeded");
                       } else {
                         resolve("Success");
@@ -102,7 +119,7 @@ const SampleTemplate = () => {
                 maxCount={1}
                 beforeUpload={(file) => {
                   return new Promise((resolve, reject) => {
-                    if (file.size > 1000000) {
+                    if (file.size > 2000000) {
                       reject("File size exceeded");
                     } else {
                       resolve("Success");
@@ -150,7 +167,7 @@ const SampleTemplate = () => {
           <Button
             type="primary"
             icon={refreshLoading ? <LoadingOutlined /> : <RedoOutlined />}
-            onClick={getImages}
+            // onClick={getImages}
           >
             Refresh
           </Button>
@@ -162,11 +179,11 @@ const SampleTemplate = () => {
                 src={e.image}
                 alt={e.image}
                 width={"100%"}
-                onClick={() => handleImageClick(e.image)}
+                // onClick={() => handleImageClick(e.image)}
               />
               <div
                 className="templateImageDeleteIcon"
-                onClick={() => handleDeleteImage(e.id, e.image)}
+                // onClick={() => handleDeleteImage(e.id, e.image)}
               >
                 <DeleteOutlined />
               </div>
