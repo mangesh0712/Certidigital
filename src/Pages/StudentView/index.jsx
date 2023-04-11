3eb03d5629a3ef2ab1d02a8ec9b201f1017f1ef8
Pankaj Kumar Ram <pankajkr885@gmail.com>
2023-04-12 03:13:10 +0530

    Started working on Share button Modal

--- src/Pages/StudentView/index.jsx
+++ src/Pages/StudentView/index.jsx
@@ -36,9 +36,11 @@ function Studentview() {
     <div>
       <HamburgerNavbar />
       {Array.isArray(data) && data.length > 0 ? (
-        <h3 style={{ textAlign: "center" }}>Certificates you have achieved</h3>
+        <h3 style={{ textAlign: "center", height: "6vh" }}>
+          Certificates you have achieved
+        </h3>
       ) : (
-        <h3 style={{ textAlign: "center",height:"6vh" }}>
+        <h3 style={{ textAlign: "center", height: "6vh" }}>
           We do not have any certificate associated with your email address.
         </h3>
       )}
@@ -52,38 +54,39 @@ function Studentview() {
           minHeight: "60.8vh",
         }}
       >
-        {Array.isArray(data) && data.length > 0
-          ? data.map((item) => (
-              <Card
-                hoverable
-                style={{
-                  width: 350,
-                  height: 300,
-                }}
-                cover={
-                  <Image
-                    src={`http://localhost:8080/student/certificateimages/${item.id}`}
-                    alt={`${item.name}`}
-                    preview={{
-                      mask: (
-                        <div style={{ background: "rgba(0, 0, 0, 0.5)" }} />
-                      ),
-                    }}
-                  />
-                }
-              >
-                <div style={{ display: "flex", gap: "100px" }}>
-                  <Meta title={item.name} />
-                  <Button>Share</Button>
-                </div>
-              </Card>
-            ))
-          : null
-            // <div style={{width:"100%"}}>
-            //   <h3 style={{ textAlign: "center" }}>
-            //     We do not have any certificate associated with your email address.
-            //   </h3>
-            // </div>
+        {
+          Array.isArray(data) && data.length > 0
+            ? data.map((item) => (
+                <Card
+                  hoverable
+                  style={{
+                    width: 350,
+                    height: 300,
+                  }}
+                  cover={
+                    <Image
+                      src={`http://localhost:8080/student/certificateimages/${item.id}`}
+                      alt={`${item.name}`}
+                      preview={{
+                        mask: (
+                          <div style={{ background: "rgba(0, 0, 0, 0.5)" }} />
+                        ),
+                      }}
+                    />
+                  }
+                >
+                  <div style={{ display: "flex", gap: "100px" }}>
+                    <Meta title={item.name} />
+                    <Button>Share</Button>
+                  </div>
+                </Card>
+              ))
+            : null
+          // <div style={{width:"100%"}}>
+          //   <h3 style={{ textAlign: "center" }}>
+          //     We do not have any certificate associated with your email address.
+          //   </h3>
+          // </div>
         }
       </div>
       <Footer />
@@ -91,3 +94,40 @@ function Studentview() {
   );
 }
 export default Studentview;
+
+
+{/* <Button onClick={showModal}>Share</Button>
+<Modal
+  open={visible}
+  onCancel={handleCancel}
+  footer={null}
+  title="Share Certificate"
+  backdropStyle={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+>
+  <Row gutter={[16, 16]}>
+    <Col span={8}>
+      <FacebookShareButton
+        url={`http://localhost:8080/student/certificateimages/${item.id}`}
+        quote={item.name}
+      >
+        Facebook
+      </FacebookShareButton>
+    </Col>
+    <Col span={8}>
+      <TwitterShareButton
+        url={`http://localhost:8080/student/certificateimages/${item.id}`}
+        title={item.name}
+      >
+        Twitter
+      </TwitterShareButton>
+    </Col>
+    <Col span={8}>
+      <LinkedinShareButton
+        url={`http://localhost:8080/student/certificateimages/${item.id}`}
+        title={item.name}
+      >
+        LinkedIn
+      </LinkedinShareButton>
+    </Col>
+  </Row>
+</Modal> */}
